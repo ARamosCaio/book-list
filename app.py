9af957bac81d4762db57ab4171ebdbec0b6c906a
Caio Ramos <aramoscaio@gmail.com>
2024-04-07 00:22:24 -0300

    feat: added virtual env, git ignore, flask setup and vue setup

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+if __name__ == "__main__":
+    app.run()
