74f5123bf5913640b89ce583319456bb798fe88e
Caio Ramos <aramoscaio@gmail.com>
2024-04-07 13:38:09 -0300

    fix: added missing cors handler

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+CORS(app, resources={r'/*': {'origins' : '*'}})
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
